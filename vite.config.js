e941d328b7c45eb2223ea47237def1f9455687ce
franciscovmn <fvmaianeto@gmail.com>
2025-02-03 19:59:41 -0300

    alteracoes vite

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/visionvest',
-  build: {
-    rollupOptions: {
-      input: {
-        index: './index.html',
-        agressivo: './src/pages/agressivo.html',
-        conservador: './src/pages/conservador.html',
-        moderado: './src/pages/moderado.html',
-        qualseuperfil: './src/pages/qualseuperfil.html',
-        calculadora_cripto: './src/pages/calculadora_conversor_cripto.html',
-        calculadora_reserva: './src/pages/calculadora_reserva_de_emergencia.html',
-        calculadora_poupanca_selic: './src/pages/calculadora_poupança_selic.html',
-        calculadora_juroscompostos: './src/pages/calculadora_juroscompostos.html'
-      },
-    },
+  base: '/visionvest/',
+  assetsInclude: ["**/*.html"],
+  server: {
+    port: 8080,
   },
-});
+  build: {
+    outDir: 'dist',
+  },
+
+  plugins: [
+    tailwindcss(),
+  ],
+})
