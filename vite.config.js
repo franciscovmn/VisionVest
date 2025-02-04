9fb77cec4d2bfb081b13d529b7fb84b20eea752e
franciscovmn <fvmaianeto@gmail.com>
2025-02-04 11:09:03 -0300

    tentando solucionar vite config js

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/visionvest',
+  build: {
+    rollupOptions: {
+      input: {
+        main: '../../index.html',
+        agressivo: './src/pages/agressivo.html',
+        conservador: './src/pages/conservador.html',
+        moderado: './src/pages/moderado.html',
+        qualseuperfil: './src/pages/qualseuperfil.html',
+        calculadora_cripto: './src/pages/calculadora_conversor_cript.html',
+        calculadora_reserva: './src/pages/calculadora_reserva_de_emergencia.html',
+        calculadora_poupanca_selic: './src/pages/calculadora_poupança_selic.html',
+        calculadora_juroscompostos: './src/pages/calculadora_juroscompostos.html'
+      },
+    }
+  },
+});
